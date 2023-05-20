f58891cbfce5b9343fbe2a0820459beeb2c9db96
Arthur <arthur.moreira@itec.ufpa.br>
2023-05-20 20:30:32 -0300

    auth - aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,22 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
+import jwt from '@fastify/jwt'
+import cors from '@fastify/cors'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'shj2h9Hi42j$>scb092/o9o',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
